2bc6623e0f4d0d394875c647603825f41922dbda
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-24 15:53:41 -0700

    Added private routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import FriendsList from './components/FriendsList';
 import AddFriend from './components/AddFriend';
 import Logout from './components/Logout';
 
+import PrivateRoute from './components/PrivateRoute';
+
 function App() {
   return (
     <Router>
@@ -27,17 +29,10 @@ function App() {
           <Login />
         </Route>
 
-        <Route exact path='/friends'>
-          <FriendsList />
-        </Route>
+        <PrivateRoute exact path='/friends' component={FriendsList}/>
+        <PrivateRoute exact path='/friends/add' component={AddFriend}/>
+        <PrivateRoute exact path='/logout' component={Logout}/>
 
-        <Route exact path='/friends/add'>
-          <AddFriend />
-        </Route>
-
-        <Route exact path='/logout'>
-          <Logout />
-        </Route>
       </div>
     </Router>
   );
