2319a8a98b671437ce4fc9c019423b2dd0bb713c
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-24 14:31:51 -0700

    Added header with links in menu bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
@@ -10,6 +10,14 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <header>
+          <h1>Friends Database</h1>
+          <Link className='link' to='/login'>Log In</Link>
+          <Link className='link' to='/friends'>Friends List</Link>
+          <Link className='link' to='/friends/add'>Add Friend</Link>
+          <Link className='link' to='/login'>Log Out</Link>
+        </header>
+
         <Route exact path='/'>
           <Login />
         </Route>
